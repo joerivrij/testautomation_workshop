7dd03ee2b226fd3c87f1bc8b746aeb3e2df944ea
Joeri Vrijaldenhoven <joeri.vrijaldenhoven@ictu.nl>
2018-12-18 16:29:09 +0100

    added CORS on proxy

--- v1/backend_proxy/manage.py
+++ v1/backend_proxy/manage.py
@@ -1,10 +1,12 @@
 from flask_script import Manager
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 from project import create_app
 
 
 app = create_app()
+CORS(app)
 jwt = JWTManager(app)
 manager = Manager(app)
 
