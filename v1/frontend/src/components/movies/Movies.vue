d12b413d462658938282fcca316bbcc592b0724c
Johan van Iperen <johan@johanvaniperen.nl>
2018-12-20 15:10:02 +0100

    update vue

--- v1/frontend/src/components/movies/Movies.vue
+++ v1/frontend/src/components/movies/Movies.vue
@@ -3,20 +3,41 @@
     <v-header></v-header>
     <section class="cta__container jumbotron text-center">
       <hr>
-      <router-link tag="button" :to="movies"></router-link>
     </section>
     <main class="movies">
+      <ul>
+        <li v-for="movie in movies" :key="movie">
+          <h3>{{movie.title}}</h3>
+          <p>{{movie.description}}</p>
+        </li>
+      </ul>
     </main>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Movies',
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
+      movies: [],
     };
+  },
+  methods: {
+    getAllMovies() {
+      axios.get('http://localhost:8080/v1/proxy/movies/').then((res) => {
+        console.info('alle films');
+        this.movies = res.data;
+        console.info(this.movies);
+      });
+    },
+  },
+  mounted() {
+    console.info('mounted');
+    this.getAllMovies();
   },
 };
 </script>
--- v1/frontend/src/components/users/SignUp.vue
+++ v1/frontend/src/components/users/SignUp.vue
@@ -3,27 +3,51 @@
     <v-header></v-header>
     <section class="login__container jumbotron text-center">
       <label for="username">Username</label>
-      <input type="text" id="username" class="login__username">
+      <input type="text"
+             id="username"
+             class="login__username"
+             v-model="userName">
       <label for="password">Password</label>
-      <input type="password" id="password" class="login__password">
-      <label for="repeat-password">Password</label>
-      <input type="password" id="repeat-password" class="login__password">
+      <input type="password"
+             id="password"
+             class="login__password"
+              v-model="password">
+      <label for="repeat-password">Repeat Password</label>
+      <input type="password"
+             id="repeat-password"
+             class="login__password"
+              v-model="confirmPassword">
       <button class="login__submit" @click="signUp">Sign Up</button>
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'SignUp',
   data() {
     return {
+      submitUrl: 'http://localhost:8080/v1/proxy/users',
       msg: 'Welcome to Your Vue.js App',
+      username: '',
+      password: '',
+      confirmPassword: '',
     };
   },
   methods: {
     signUp() {
-      console.info('Ik geef me op!');
+      const formData = {
+        userName: this.userName,
+        password: this.password,
+        confirmPassword: this.confirmPassword,
+      };
+      console.info(formData);
+      // todo, fix CORS issue with OPTIONS call and allow-origin *
+      axios.post(this.submitUrl, formData)
+        .then(res => console.info(res))
+        .catch(error => console.error(error));
     },
   },
 };
