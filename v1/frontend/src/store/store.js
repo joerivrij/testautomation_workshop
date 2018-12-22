4616a469342931a72d1b00d01def9c67f2b51e49
Johan van Iperen <johan@johanvaniperen.nl>
2018-12-22 15:52:38 +0100

    added autologin

--- v1/frontend/src/store/store.js
+++ v1/frontend/src/store/store.js
@@ -7,16 +7,19 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     token: '',
+    userId: null,
     proxyUrl: 'http://localhost:8080',
     loginUrl: 'http://localhost:8080/v1/proxy/tokens/',
     signUpUrl: 'http://localhost:8080/v1/proxy/users/',
     moviesUrl: '/v1/proxy/movies/',
     loggedIn: false,
+    loginExpires: null,
     currentMovie: {},
   },
   mutations: {
-    setToken: (state, token) => {
-      state.token = token;
+    setToken: (state, auth) => {
+      state.token = auth.token;
+      state.userId = auth.userId;
     },
     setCurrentMovie: (state, movie) => {
       state.currentMovie = movie;
@@ -25,6 +28,9 @@ export default new Vuex.Store({
   actions: {
     logOut: ({ state }) => {
       state.token = '';
+      localStorage.setItem('token', null);
+      localStorage.setItem('userId', null);
+      localStorage.setItem('expirationDate', null);
       return true;
     },
     loginWithCredentials: ({ commit, state }, credentials) => {
@@ -40,13 +46,32 @@ export default new Vuex.Store({
       axios.post(state.loginUrl, cred, config)
         .then((res) => {
           const token = res.data.access_token;
-          commit('setToken', token);
+          const expirationDate = new Date(Date.now() + (res.data.expires_in * 1000));
+          commit('setToken', { token, userId: cred.id });
+          localStorage.setItem('token', JSON.stringify(token));
+          localStorage.setItem('userId', JSON.stringify(cred.id));
+          localStorage.setItem('expirationDate', JSON.stringify(expirationDate));
           return true;
         })
         .catch((error) => {
           console.error(error);
           return false;
         });
+    },
+    tryAutoLogin({ commit }) {
+      const token = JSON.parse(localStorage.getItem('token'));
+      if (!token) {
+        return false;
+      }
+      const expirationDate = JSON.parse(localStorage.getItem('expirationDate'));
+      const now = new Date();
+      if (now >= expirationDate) {
+        return false;
+      }
+      const userId = JSON.parse(localStorage.getItem('userId'));
+      commit('setToken', { token, userId });
+      console.info('Joepie auto ingelogd!');
+      return true;
     },
     getMovie: ({ commit, state }, imdbId) => {
       const axiosInstance = axios.create({
